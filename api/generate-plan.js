e549baa80ecc5f5265e27301bf5c2ba99e46dc01
Surwix <surwixsupply@gmail.com>
2025-07-21 21:51:56 -0400

    Update generate-plan.js

--- api/generate-plan.js
+++ api/generate-plan.js
@@ -1,7 +1,4 @@
 export default async function handler(request, response) {
-    // Эта строка - единственное, что мы хотим увидеть
     console.log("API Test: Function was successfully invoked at " + new Date());
-    
-    // Мгновенно отправляем ответ
     response.status(200).json({ message: "Test successful! The API endpoint is working." });
 }
